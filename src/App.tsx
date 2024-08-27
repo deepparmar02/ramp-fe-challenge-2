7d099bf636beac58dcfae6f572638eb7a47450f5
Deep Parmar <66918584+deepparmar02@users.noreply.github.com>
2024-08-27 22:18:54 +0000

    bug 4

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export function App() {
             console.log(newValue)
             if (newValue === null) {
               return
-            } else if (newValue.id == "") {
+            } else if (newValue.id === "") {
               await loadAllTransactions()
               return
             }
